aa99bc58bdd34a16aeb8514cc67b167b2b006afc
alexr942 <alexrobertson518@gmail.com>
2021-01-14 18:08:53 +0000

    jamming react app

---
+++ src/Components/tracklist/tracklist.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './tracklist.css';
+
+import Track from '../track/track';
+
+class TrackList extends React.Component {
+    render() {
+        return (
+            <div className="TrackList">
+                {
+                     this.props.tracks.map(track => {
+                        return <Track track={track}
+                                     Key={track.id} 
+                                     onAdd={this.props.onAdd}
+                                     onRemove={this.props.onRemove}
+                                     isRemoval={this.props.isRemoval}/>
+                    })
+                }
+            </div>
+        )
+    }
+}
+
+export default TrackList;
